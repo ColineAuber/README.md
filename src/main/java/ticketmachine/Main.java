dfa5a934d6bc0fec96f871c23e17776863935c26
Coline <colineauber@yahoo.fr>
2022-10-19 12:38:47 +0200

    hahaha

--- src/main/java/ticketmachine/Main.java
+++ src/main/java/ticketmachine/Main.java
@@ -4,6 +4,7 @@ public class Main {
 // anniv melanie
 	// merci beaucoup ;)
 	//c aussi l'anniv de jeannnnn! bon anniversaire à vous 2
+	// c'est moi mélanie même si c'est sur coline
 	public static void main(String[] args) {
 		TicketMachine machine = new TicketMachine(50);
 		System.out.println("L'utilisateur insère 60 centimes");
