d21e35cd0f3cdcc692b38d854ad160e90d33f71b
lucilledelbos <116156710+lucilledelbos@users.noreply.github.com>
2022-10-19 12:38:06 +0200

    Update Main.java

--- src/main/java/ticketmachine/Main.java
+++ src/main/java/ticketmachine/Main.java
@@ -4,6 +4,7 @@ public class Main {
 // anniv melanie
 	// merci beaucoup ;)
 	//c aussi l'anniv de jeannnnn! bon anniversaire à vous 2
+	// de rien hein?
 	public static void main(String[] args) {
 		TicketMachine machine = new TicketMachine(50);
 		System.out.println("L'utilisateur insère 60 centimes");
